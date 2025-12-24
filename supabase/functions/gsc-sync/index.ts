869bf1dce2947dfb01f26aaa57b8635c2d96b7d4
Simón Sandrea <simonsandrea.seo@gmail.com>
2025-12-24 03:43:26 -0400

    fix(gsc-sync): update secret name to SERVICE_ROLE_KEY

--- supabase/functions/gsc-sync/index.ts
+++ supabase/functions/gsc-sync/index.ts
@@ -17,7 +17,7 @@ serve(async (req) => {
         // 1. Init Supabase Admin Client
         const supabaseAdmin = createClient(
             Deno.env.get('SUPABASE_URL') ?? '',
-            Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+            Deno.env.get('SERVICE_ROLE_KEY') ?? ''
         )
 
         // 2. Get Project & Owner
